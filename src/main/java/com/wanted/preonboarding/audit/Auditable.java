b9fc0d6976c417365cb466555f5fd63d49d81e59
minoolian <mw.lee@kakao.com>
2023-08-10 20:07:50 +0900

    feat: Auditing 객체 추가

---
+++ src/main/java/com/wanted/preonboarding/audit/Auditable.java
@@ -0,0 +1,21 @@
+package com.wanted.preonboarding.audit;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+
+    @CreatedDate
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedTime;
+}
