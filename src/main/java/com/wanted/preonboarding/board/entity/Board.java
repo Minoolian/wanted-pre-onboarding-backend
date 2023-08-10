0f7c5b342bfe2fe4e8a641802a8f460bcd4d09fb
minoolian <mw.lee@kakao.com>
2023-08-10 20:08:06 +0900

    feat: Board Entity 추가

---
+++ src/main/java/com/wanted/preonboarding/board/entity/Board.java
@@ -0,0 +1,19 @@
+package com.wanted.preonboarding.board.entity;
+
+import com.wanted.preonboarding.audit.Auditable;
+import jakarta.persistence.*;
+
+@Entity
+public class Board extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+}
