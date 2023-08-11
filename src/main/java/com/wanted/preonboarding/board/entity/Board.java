6a91013b30c22311f89818011d39c1992242dfcd
minoolian <mw.lee@kakao.com>
2023-08-11 18:47:40 +0900

    fix: Board Entity 생성자 수정

--- src/main/java/com/wanted/preonboarding/board/entity/Board.java
+++ src/main/java/com/wanted/preonboarding/board/entity/Board.java
@@ -3,12 +3,16 @@ package com.wanted.preonboarding.board.entity;
 import com.wanted.preonboarding.audit.Auditable;
 import com.wanted.preonboarding.board.dto.BoardPatchDto;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Board extends Auditable {
 
     @Id
