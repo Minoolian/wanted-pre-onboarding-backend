e5660f0a87ea20acf11c4a84f9fce8fb9afbe3de
minoolian <mw.lee@kakao.com>
2023-08-11 16:49:56 +0900

    feat: Board 게시글 목록 조회 DTO 추가

---
+++ src/main/java/com/wanted/preonboarding/board/dto/BoardGetDto.java
@@ -0,0 +1,14 @@
+package com.wanted.preonboarding.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BoardGetDto {
+
+    private Long boardId;
+
+    private String title;
+
+}
