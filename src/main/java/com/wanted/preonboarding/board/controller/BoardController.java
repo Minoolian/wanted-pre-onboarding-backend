58e38a78c9ce124bf331563064ca559a9f1c95cd
minoolian <mw.lee@kakao.com>
2023-08-11 16:05:29 +0900

    feat: Board 게시글 단건 조회 Get 핸들러 메서드 추가

--- src/main/java/com/wanted/preonboarding/board/controller/BoardController.java
+++ src/main/java/com/wanted/preonboarding/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.wanted.preonboarding.board.controller;
 
+import com.wanted.preonboarding.board.dto.BoardGetDetailDto;
 import com.wanted.preonboarding.board.dto.BoardPostDto;
 import com.wanted.preonboarding.board.entity.Board;
 import com.wanted.preonboarding.board.mapper.BoardMapper;
@@ -7,10 +8,7 @@ import com.wanted.preonboarding.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +24,13 @@ public class BoardController {
         Board board = boardMapper.boardPostDtoToBoard(boardPostDto);
         Board createdBoard = boardService.createBoard(board);
         return new ResponseEntity<>(createdBoard.getBoardId(), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{boardId}")
+    public ResponseEntity<BoardGetDetailDto> readBoard(@PathVariable Long boardId) {
+        Board board = boardService.readBoard(boardId);
+
+        return new ResponseEntity<>(boardMapper.boardToBoardGetDetailDto(board), HttpStatus.OK);
     }
 
 }
