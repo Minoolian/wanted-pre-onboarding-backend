829f27970a57e09f9871ab1d879f20c5937db91c
minoolian <mw.lee@kakao.com>
2023-08-11 16:48:16 +0900

    feat: Board-BoardGetDTO 변환 Mapper 생성
    * List간 변환 mapper도 추가로 생성

--- src/main/java/com/wanted/preonboarding/board/mapper/BoardMapper.java
+++ src/main/java/com/wanted/preonboarding/board/mapper/BoardMapper.java
@@ -1,9 +1,12 @@
 package com.wanted.preonboarding.board.mapper;
 
 import com.wanted.preonboarding.board.dto.BoardGetDetailDto;
+import com.wanted.preonboarding.board.dto.BoardGetDto;
 import com.wanted.preonboarding.board.dto.BoardPostDto;
 import com.wanted.preonboarding.board.entity.Board;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface BoardMapper {
@@ -11,4 +14,8 @@ public interface BoardMapper {
     Board boardPostDtoToBoard(BoardPostDto boardPostDto);
 
     BoardGetDetailDto boardToBoardGetDetailDto(Board board);
+
+    BoardGetDto boardToBoardGetDto(Board board);
+
+    List<BoardGetDto> boardsToBoardGetDTOs(List<Board> content);
 }
