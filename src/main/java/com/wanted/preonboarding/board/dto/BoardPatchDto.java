396551b6bb45a2b01ed996e99f8d594087445050
minoolian <mw.lee@kakao.com>
2023-08-11 18:47:47 +0900

    fix: Board PatchDTO 생성자 수정

--- src/main/java/com/wanted/preonboarding/board/dto/BoardPatchDto.java
+++ src/main/java/com/wanted/preonboarding/board/dto/BoardPatchDto.java
@@ -1,8 +1,10 @@
 package com.wanted.preonboarding.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class BoardPatchDto {
 
     private String title;
