86435848fb56051ba0b980a8d4c9472ee6db10b9
minoolian <mw.lee@kakao.com>
2023-08-16 19:52:59 +0900

    feat: User Post 요청(signup) DTO 추가

---
+++ src/main/java/com/wanted/preonboarding/user/dto/UserPostDto.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboarding.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserPostDto {
+
+    private String email;
+
+    private String password;
+
+    private String username;
+}
