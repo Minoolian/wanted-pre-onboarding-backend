6bc9f85c26470389a4950764c8927cd0e4b4020a
minoolian <mw.lee@kakao.com>
2023-08-16 22:04:51 +0900

    feat: 본인확인 ExceptionCode 추가

--- src/main/java/com/wanted/preonboarding/exception/ExceptionCode.java
+++ src/main/java/com/wanted/preonboarding/exception/ExceptionCode.java
@@ -5,7 +5,8 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ExceptionCode {
-    BOARD_NOT_FOUND(404, "게시글이 존재하지 않습니다.");
+    BOARD_NOT_FOUND(404, "게시글이 존재하지 않습니다."),
+    UNAUTHORIZATION(403, "본인이 작성한 게시물이 아닙니다.");
 
     @Getter
     final int status;
