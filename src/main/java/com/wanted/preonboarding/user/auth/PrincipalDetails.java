c5879d8e80e0cd24b7b21add116daff3f0c8ffaf
minoolian <mw.lee@kakao.com>
2023-08-16 19:56:10 +0900

    feat: Authentication에 담길 UserDetails 객체 생성

---
+++ src/main/java/com/wanted/preonboarding/user/auth/PrincipalDetails.java
@@ -0,0 +1,59 @@
+package com.wanted.preonboarding.user.auth;
+
+import com.wanted.preonboarding.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    private Collection<GrantedAuthority> authorities;
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static PrincipalDetails create(User user) {
+        return new PrincipalDetails(
+                user,
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getRole()))
+        );
+    }
+}
