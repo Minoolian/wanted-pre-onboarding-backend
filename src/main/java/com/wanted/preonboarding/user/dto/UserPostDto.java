af794bf31cf77259e4fcb7da8917d65f22bb2a36
minoolian <mw.lee@kakao.com>
2023-08-16 22:01:07 +0900

    feat: 회원가입시 유효성 검증 추가

--- src/main/java/com/wanted/preonboarding/user/dto/UserPostDto.java
+++ src/main/java/com/wanted/preonboarding/user/dto/UserPostDto.java
@@ -1,12 +1,16 @@
 package com.wanted.preonboarding.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserPostDto {
 
+    @Email(message = "@를 포함한 Email 양식이어야 합니다.")
     private String email;
 
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
 
     private String username;
