2be254287ebebe1642d8899f3d5c5c9849d9704d
minoolian <mw.lee@kakao.com>
2023-08-16 19:51:38 +0900

    feat: User Repository 추가

---
+++ src/main/java/com/wanted/preonboarding/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.wanted.preonboarding.user.repository;
+
+import com.wanted.preonboarding.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
